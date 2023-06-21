f4af9d4b95afb8f2fef1d25e802d713941dcef17
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-06-20 21:24:50 -0300

    update Welcome

--- Client/src/Components/Welcome/Welcome.jsx
+++ Client/src/Components/Welcome/Welcome.jsx
@@ -1,18 +1,15 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
-import { useSelector } from "react-redux";
 import logo from "../../../images/logo.png";
+import welcomeImage from "../../../images/welcomeImage.png";
 
 export default function Welcome() {
-  const probar = useSelector((state) => state.ReducerAuth.usuarioAuth);
-  console.log(probar);
   return (
     <>
-      <View className='mt-32'>
-        <Text>Welcome</Text>
-      </View>
+      <View className="mt-32"></View>
       <View>
         <Image source={logo} />
+        <Image source={welcomeImage} />
       </View>
     </>
   );
