89b58c6a5c29879ffdf50da05145a94ed9fbddbd
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-06-21 18:38:56 -0300

    welcome update

--- Client/src/Components/Welcome/Welcome.jsx
+++ Client/src/Components/Welcome/Welcome.jsx
@@ -2,7 +2,8 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import logo from "../../../images/logo.png";
 import welcomeImage from "../../../images/welcomeImage.png";
-import Wuau from "../../icons/Wuau";
+import wuau from "../../../images/wuau.png";
+
 import Button from "../Buttons/Button";
 
 export default function Welcome() {
@@ -11,23 +12,15 @@ export default function Welcome() {
   };
 
   return (
-    <>
-      <View className="mt-24">
-        <View>
-          <Image source={logo} />
-          <Image source={welcomeImage} />
-        </View>
-        <View className="mt-5">
-          <Wuau />
-        </View>
-      </View>
-      <View className="flex-1 items-center justify-center">
-        <View>
-          <Button title="Test" onPress={testButton} />
-          <View className="my-2" />
-          <Button title="Test" onPress={testButton} />
-        </View>
-      </View>
-    </>
+    <View className="flex-1 items-center justify-center">
+      <Image source={logo} />
+      <Image source={welcomeImage} />
+
+      <Image source={wuau} />
+
+      <Button title="Test" onPress={testButton} />
+
+      <Button title="Test" onPress={testButton} />
+    </View>
   );
 }
