aa954b55fd1072213f28c2584ca94e4cfe56bebf
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-06-23 13:55:28 -0300

    update

--- Client/src/Components/ResetPassword/Reset1Step.jsx
+++ Client/src/Components/ResetPassword/Reset1Step.jsx
@@ -1,46 +1,62 @@
 import React from "react";
 import { View, Image, TextInput, Text } from "react-native";
 import Button from "../Buttons/Button";
+import leftIcon from "../../../images/leftIcon.png";
 
-export const Reset1Step = ( { email, 
-    setEmail, 
-    password, 
-    setPassword, steps, setSteps, emailPassword, verification}) =>{
+export const Reset1Step = ({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  steps,
+  setSteps,
+  emailPassword,
+  verification,
+}) => {
+  const Code = async () => {
+    try {
+      await emailPassword();
+      setSteps(1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const Code = () =>{
-    emailPassword().then(() =>  setSteps(1)).cath((err) => console.log(err))
-   
-}
-
-    return(
-        <>
-         <View className="flex-1 items-center justify-center bg-white">
-          <Text className="font-poppins text-center py-12">
-          Escribe el email asociado a tu cuenta y te enviaremos un código para recuperar tu cuenta
+  return (
+    <View className="flex-1 items-center justify-start pt-10 bg-white">
+      <View className="flex flex-row items-center mb-32">
+        <Image source={leftIcon} className="ml-6" />
+        <View className="flex-1 justify-center">
+          <Text className="text-center text-base font-poppinsBold mr-16">
+            Iniciar sesión
           </Text>
+        </View>
+      </View>
+      <Text className="text-center mb-8 px-8 font-poppins">
+        Escribe el email asociado a tu cuenta y te enviaremos un código para
+        recuperar tu cuenta
+      </Text>
 
-        <View className="w-4/5 ">
-          <TextInput
-            placeholder=""
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            className="w-full rounded-full bg-naranja  px-4 mb-4 h-14"
-          />
-          </View>
-                <View className="flex mt-16">
-
-
+      <View className="w-4/5">
+        <TextInput
+          placeholder="   Email"
+          placeholderTextColor="white"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          className="w-full rounded-full bg-naranja px-4 mb-4 h-14 font-poppinsBold"
+        />
+      </View>
+      <View className="flex mt-12">
         <Button
           title="Recuperar"
-          onPress={() => emailPassword()}
+          onPress={() => Code()}
           colorButton="bg-black"
           colorText="text-white"
-          ancho="w-72"
-          alto="h-14"
-          textSize="text-lg"
+          ancho="w-40"
+          alto="h-11"
+          textSize="text-base"
         />
       </View>
     </View>
-        </>
-    )
-}
+  );
+};
--- Client/src/Components/ResetPassword/Reset3Step.jsx
+++ Client/src/Components/ResetPassword/Reset3Step.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { View, Text } from "react-native";
 
-export const Reset3Step = () =>{
-
-    return(
-        <>aca cambuia la contraseña</>
-    )
-}
+export const Reset3Step = () => {
+  return (
+    <View>
+      <Text>acá cambuia la contraseña</Text>
+    </View>
+  );
+};
