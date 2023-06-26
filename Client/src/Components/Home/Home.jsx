bbd05d052d3a519745920cfa322dfc37b7033f63
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-06-25 23:31:07 -0300

    update home

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -10,27 +10,27 @@ export default function Home() {
       imagen: juguetePerro,
       nombre: "Producto 1",
       descripcion: "Descripción del Producto 1",
-      precio: "$10.99",
+      precio: "10.99€",
     },
     {
       id: 2,
       imagen: juguetePerro,
       nombre: "Producto 2",
       descripcion: "Descripción del Producto 2",
-      precio: "$19.99",
+      precio: "19.99€",
     },
     {
       id: 3,
       imagen: juguetePerro,
       nombre: "Producto 3",
       descripcion: "Descripción del Producto 3",
-      precio: "$14.99",
+      precio: "14.99€",
     },
   ];
 
   return (
     <View className="flex w-full h-full">
-      <View className="flex h-32 w-fit mt-28 mx-10 rounded bg-naranja">
+      <View className="flex h-32 w-fit mt-14 mx-10 rounded-md bg-naranja">
         <View className="flex w-8/12 ml-4 mt-3">
           <Text className="text-base font-poppinsBold  text-white">
             Tu mascota siempre segura con Whopaws
@@ -54,23 +54,35 @@ export default function Home() {
           </View>
         </View>
       </View>
-      <Image source={chip} className="absolute ml-60 mt-16" />
+      <Image source={chip} className="absolute ml-60 mt-2" />
 
-      <View className="mt-10 ml-10">
-        <Text className="text-2xl font-bold">Productos destacados</Text>
-      </View>
-      <View className="mt-10">
-        {productosDestacados.map((producto) => (
-          <View
-            key={producto.id}
-            className="bg-naranja m-2 p-4 rounded shadow-md"
-          >
-            <Image source={producto.imagen} className="w-28 h-28" />
-            <Text className="text-lg font-bold mt-4">{producto.nombre}</Text>
-            <Text className="text-base">{producto.descripcion}</Text>
-            <Text className="text-lg font-bold mt-4">{producto.precio}</Text>
-          </View>
-        ))}
+      <View className="mt-10 ml-4">
+        {/* View container de Productos destacados */}
+
+        <Text className="text-sm font-poppinsBold">Productos destacados</Text>
+
+        <View className="flex flex-row flex-wrap mt-5">
+          {productosDestacados.map((producto) => (
+            <View
+              key={producto.id}
+              className=" m-1 w-28 h-44 rounded-lg shadow-md bg-naranja"
+            >
+              <Image
+                source={producto.imagen}
+                className="w-24 h-24 m-2 rounded-lg"
+              />
+              <Text
+                className="mx-1 font-poppins text-white"
+                style={{ fontSize: 6.7 }}
+              >
+                {producto.nombre}. {producto.descripcion}
+              </Text>
+              <Text className="mx-1 text-sm font-poppinsBold text-white">
+                {producto.precio}
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
