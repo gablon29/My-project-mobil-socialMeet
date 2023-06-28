fbfb42074d6e002651352a04c07d9b2723c8ab78
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-06-27 22:57:55 -0300

    update Header Notifications

--- Client/src/metodos/petsMetodos.js
+++ Client/src/metodos/petsMetodos.js
@@ -1,27 +1,57 @@
-export const CreatePet = async (
-   token, info
-  ) => {
-    try {
-      const response = await fetch("https://whopaws-production.up.railway.app/api/pet/", { //crea una mascota
+export const CreatePet = async (token, info) => {
+  try {
+    const response = await fetch(
+      "https://whopaws-production.up.railway.app/api/pet/",
+      {
+        //crea una mascota
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-          info
+          info,
         }),
-      });
-  
-      if (!response.ok) {
-        throw new Error("login failed");
       }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-      throw error;
+    );
+
+    if (!response.ok) {
+      throw new Error("login failed");
     }
-  };
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const GetPets = async (token, info) => {
+  try {
+    const response = await fetch(
+      "https://whopaws-production.up.railway.app/api/pet/",
+      {
+        //||Obtener las mascota
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          info,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("login failed");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
