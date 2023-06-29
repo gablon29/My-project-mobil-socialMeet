f1a5f4171403a48393216ea62cdecdb9d4b0c8fa
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-06-30 01:29:07 +0200

    Notifications update

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
         await axios.post('https://whopaws-production.up.railway.app/api/send/send-notification', notification);
         console.log('Notificación enviada');
       } catch (error) {
-        console.error('Error al enviar la notificación:', error);
+        console.error('Error al enviar la notificación:dc', error);
       }
     };
 
