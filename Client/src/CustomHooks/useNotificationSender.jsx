90a968a810018935a867874c68f4b7e419d0d420
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-06-30 13:48:02 -0400

    Revert "Revert "Merge pull request #65 from iXGEN/igna""
    This reverts commit 2ad95bd5c796ea56f3433da77ace6b27b48abdea.

--- Client/src/CustomHooks/useNotificationSender.jsx
+++ Client/src/CustomHooks/useNotificationSender.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
-import axios from 'axios';
+import axios from "axios";
 
 export const useNotificationSender = () => {
-    const sendNotification = async (token, title, body) => {
-      const notification = {
-        to: token,
-        title,
-        body,
-      };
-
-      try {
-        const data = await axios.post('https://whopaws-production.up.railway.app/api/send/send-notification', notification);
-        if(data.error) throw new Error (data.message)
-        console.log('Notificación enviada');
-      } catch (error) {
-        console.error('Error al enviar la notificación:', error);
-      }
+  const sendNotification = async (token, title, body) => {
+    const notification = {
+      to: token,
+      title,
+      body,
     };
 
-    // Devolvemos el método sendNotification para poder utilizarlo desde el componente
-    return {
-      sendNotification,
-    };
+    try {
+      const data = await axios.post(
+        "https://whopaws-production.up.railway.app/api/send/send-notification",
+        notification
+      );
+      if (data.error) throw new Error(data.message);
+      console.log("Notificación enviada");
+    } catch (error) {
+      console.error("Error al enviar la notificación:", error);
+    }
+  };
 
+  // Devolvemos el método sendNotification para poder utilizarlo desde el componente
+  return {
+    sendNotification,
+  };
 };
-
