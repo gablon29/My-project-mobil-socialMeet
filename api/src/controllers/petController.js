9a2e87c583c57c25c58217f0f2da42044e2ad3f9
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-07-01 05:53:59 -0400

    Actualización Back
    -LISTAR PET TESTEO CORRECTO CON POSTMAN
    -CREAR PET YA ESTA LISTO SOLO FALTA QUE RODRI LE DE VISTO BUENO AVER QUE LE FALTA SEGUN SU VISTO BUENO

--- api/src/controllers/petController.js
+++ api/src/controllers/petController.js
@@ -2,23 +2,24 @@ const PetModel = require('../models/pet.model');
 const UserModel = require('../models/user.model');
 const { ClientError } = require('../utils/errors');
 
-
-const createPet = async (PetData, id) => {
+const createPet = async ({ name, specie, breed, weight, sex, age, health, profilePic }, id) => {
   const dueño = await UserModel.findOne({ id: id });
-  if (!dueño) throw new ClientError("Este usuario no existe en la base de datos", 400);
-  const newPet = await PetModel.create(PetData); //lo crea
-  await dueño.pets.push(newPet._id); //crea la realacion
+  if (!dueño) throw new ClientError('Este usuario no existe en la base de datos', 400);
+  // const newPet = await PetModel.create(PetData); //lo crea
+
+  const newPet = new PetModel({ name, specie, breed, weight, sex, age, health, profilePic, owner: dueño.id }); //lo crea
+
+  await newPet.save();
+  await dueño.pets.push(newPet.id); //crea la realacion
   await dueño.save(); //actualiza al usuario en la DB
   return newPet;
 };
-
 
 const updatePet = async (PetData, petID, ownerEmail) => {
   const queryCondition = { _id: petID, owner: ownerEmail };
   const updatedPet = await PetModel.updateOne(queryCondition, PetData);
   return updatedPet;
 };
-
 
 const filterByOwner = async (id) => {
   const ownerPets = await PetModel.find({ owner: id });
