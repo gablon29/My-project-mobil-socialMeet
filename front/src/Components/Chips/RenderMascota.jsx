54112be94ef2602c5f8f395d6ac373ed9392c669
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-07-04 20:42:31 -0300

    enviar para don luis

--- front/src/Components/Chips/RenderMascota.jsx
+++ front/src/Components/Chips/RenderMascota.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
 
-export const RenderMascota = ({mascota}) =>{
-
-console.log(mascota)
-    return(
-        <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">{mascota.pet.name}</h2>
-          <p>{mascota.owner.phone} {mascota.owner.firstName}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary"></button>
-          </div>
+export const RenderMascota = ({ mascota }) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{mascota && mascota.pet.name}</h2>
+        <p>
+          {mascota && mascota.owner.phone} {mascota && mascota.owner.firstName}
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary"></button>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
