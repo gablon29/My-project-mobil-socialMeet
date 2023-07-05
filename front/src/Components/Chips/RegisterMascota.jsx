d65031a423390eb8562b47ff8fdac4d039f68531
rodri <rod.toobe@gmail.com>
2023-07-05 11:45:37 -0300

    fix: front mensajes y cartas

--- front/src/Components/Chips/RegisterMascota.jsx
+++ front/src/Components/Chips/RegisterMascota.jsx
@@ -48,7 +48,9 @@ export const RegisterMascota = ({ set_quiere_logearse, petid }) => {
                   name="chipId"
                   onChange={(e) => handleChange(e)}
                 />
-              
+                <h1 className="text-2xl xl:text-3xl font-extrabold">
+              Es un chip nuevo?
+            </h1>
                 <button
                   onClick={() => handleSubmit()}
                   className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
