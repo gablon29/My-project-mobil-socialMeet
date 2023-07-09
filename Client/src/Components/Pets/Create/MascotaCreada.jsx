4a0bd63d6b4bb5d56f2354f9cbb5a93c304411e6
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-07-08 20:10:55 -0400

    Actualizacion

---
+++ Client/src/Components/Pets/Create/MascotaCreada.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MascotaCreada = () => {
+    return (
+        <View>
+          <Text>MascotaCreada</Text>
+        </View>
+      )
+};
+
+export default MascotaCreada;
