d961f9a87322c48ab98f54dfc0abf8886974c0b7
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-07-15 20:45:48 +0200

    Update authMetodos.js

--- Client/src/metodos/authMetodos.js
+++ Client/src/metodos/authMetodos.js
@@ -59,8 +59,7 @@ export const ReloadAuthMethod = async ({ loading, error, success }) => {
       .catch((err) => {
         throw new Error(err.response.data.message);
       });
-      console.log("prueba", response.data)
-    success(response);
+    success(response.data);
     loading(false);
   } catch (err) {
     console.log('ReloadAuthMethod', err);
