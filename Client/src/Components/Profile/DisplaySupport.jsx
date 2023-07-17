f8b6f111d29bc9b521b0dd49ef9dd3c72138e1a2
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-07-17 17:28:14 -0600

    Vista de ver ticket en desarrollo

--- Client/src/Components/Profile/DisplaySupport.jsx
+++ Client/src/Components/Profile/DisplaySupport.jsx
@@ -52,6 +52,7 @@ const DisplaySupport = () => {
                 alto="h-8"
                 textSize="text-sm"
                 shadow="shadow-lg shadow-black"
+                onPress={()=>navigation.navigate("ChatTikect", {item})}
             />
         </View>)
     }
