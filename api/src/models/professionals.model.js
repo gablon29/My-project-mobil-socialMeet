a930b312ee03e884ddbb21a7a6e4f0b788194527
bosxch <amparocardosobosch@gmail.com>
2023-07-24 19:44:27 -0300

    professionals model

--- api/src/models/professionals.model.js
+++ api/src/models/professionals.model.js
@@ -53,19 +53,19 @@ const professionalsSchema = mongoose.Schema(
     fee: {
         type: Number
     },
-    // experience: {
-    // type: Date 
-    // },
-    // completed: {
-    //     type: Number
-    // },
-    // reviews: {
-    //     type: Array
-    // },
-    // state: {
-    //     type: Boolean
-    //     default: false
-    // },
+    experience: {
+    type: Date 
+    },
+    completed: {
+        type: Number
+    },
+    reviews: {
+        type: Array
+    },
+    state: {
+        type: Boolean,
+        default: false,
+    },
     profilePic: {
       type: String,
       default: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
