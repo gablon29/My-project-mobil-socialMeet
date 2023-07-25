2edf65f5fa0edc433492322d966692bc9ae9c2dc
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-07-26 01:50:27 +0200

    Subir productos a stripe a medio hacer

--- api/src/controllers/stripe/index.ts
+++ api/src/controllers/stripe/index.ts
@@ -3,6 +3,7 @@ import getAllProducts from "./getAllProducts"
 import getApiKey from "./getApiKey"
 import postStartBuyProcess from "./postStartBuyProcess"
 import postHandleStripeEvents from "./postHandleStripeEvents"
+import createProducts from "./createProducts"
 
 const stripeControllers =
 {
@@ -11,7 +12,7 @@ const stripeControllers =
     postHandleStripeEvents,
     getApiKey,
     postStartBuyProcess,
-
+    createProducts
 }
 
 export default stripeControllers
