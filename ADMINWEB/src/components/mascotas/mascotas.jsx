dd1e59e48425e70dc99ad66a3d785930f6c55d61
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-07-27 19:23:50 +0200

    mascotas update

--- ADMINWEB/src/components/mascotas/mascotas.jsx
+++ ADMINWEB/src/components/mascotas/mascotas.jsx
@@ -13,11 +13,11 @@ export const Mascotas = () => {
 
 
   return (
-    <>
+    <div>
       <div className="bg-white fixed w-screen right-0 pl-[256px] z-10">
         <HeaderAdmin />
- <FilterAndSearch handleSearch={handleSearch} searchTerm={searchTerm}/>
- </div>
+        <FilterAndSearch handleSearch={handleSearch} searchTerm={searchTerm} />
+      </div>
       <div className="flex justify-between mb-4">
         <h1 className="text-2xl font-bold">Mascotas</h1>
         <input
@@ -28,30 +28,35 @@ export const Mascotas = () => {
           onChange={handleSearch}
         />
       </div>
-      <table className="table-auto bg-white text-black w-full h-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Nombre</th>
-            <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">Especie</th>
-            <th className="px-4 py-2">Editar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredUsuarios.map((mascota) => (
-            <tr key={mascota.id}>
-              <td className="px-4 py-2">{mascota.name}</td>
-              <td className="px-4 py-2">{mascota.id}</td>
-              <td className="px-4 py-2">{mascota.specie}</td>
-              <td className="px-4 py-2">
-                <button className="bg-orange-500 text-white px-4 py-2 rounded-md">
-                  Editar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </>
+      <div
+        style={{ paddingBottom: "250px" }}
+        className="h-full bg-white fixed overflow-x-auto w-screen mt-[240px] right-0 pl-[256px] z-10"
+      >
+        <div className="bg-white text-black flex justify-evenly fixed pb-5 w-screen right-0 pl-[256px] z-10 border-b-[1px] border-gray-500">
+          <input className="relative left-3" type="checkbox" />
+          <span className="block w-full text-center text-xl font-semibold">Nombre</span>
+          <span className="block w-full text-center text-xl font-semibold">ID</span>
+          <span className="block w-full text-center text-xl font-semibold">especie</span>
+          <span className="block w-full text-center text-xl font-semibold">Editar</span>
+        </div>
+        <div className="bg-white text-white w-full flex flex-col pt-14">
+          {filteredUsuarios.map((usuario) => {
+            return (
+              <div key={usuario.id} className="w-full text-black bg-white flex justify-evenly items-center py-1">
+                <input className="relative left-3" type="checkbox" />
+                <span className="w-full text-center">{usuario.name}</span>
+                <span className="w-full text-center">{usuario.id}</span>
+                <span className="w-full text-center">{usuario.specie}</span>
+                <span className="w-full text-center">
+                  <button className="bg-naranja text-white px-4 py-2 rounded-full">
+                    Ver mascota
+                  </button>
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
-};
+        }  
