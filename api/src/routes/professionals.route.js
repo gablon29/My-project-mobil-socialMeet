03621c786ffde2a31f328011a062425c9fe17704
bosxch <amparocardosobosch@gmail.com>
2023-07-28 19:15:26 -0300

    fee en stripe en registro y edición

--- api/src/routes/professionals.route.js
+++ api/src/routes/professionals.route.js
@@ -2,19 +2,40 @@ const ProfessionalModel = require('../models/professionals.model')
 const { response } = require('../utils');
 const UserModel = require('../models/user.model')
 const { sendNotifications } = require('../controllers/pushController');
+const { default: editPrice } = require('../controllers/stripe/createPrice');
+
 
 module.exports = {
   register: async (req, res) => {
-      const { description, fee, experience, userId } = req.body;
+    const { description, fee, experience, userId } = req.body;
 
-      const newProfessional = new ProfessionalModel({
-        user: userId,
-        description: description,
-        fee: fee,
-        experience: experience,
-      });
-      await newProfessional.save();
-      return response(res, 201, { message: 'Registro exitoso', professional: newProfessional });
+    const stripeProductData = {
+      name: 'FEE',
+      metadata: 'fee'
+    };
+
+    const stripeProduct = await createProducts(stripeProductData);
+
+    const separadorCentavos = fee.includes(',') ? ',' : '.';
+
+    const feeInCents = parseInt(parseFloat(fee.replace(separadorCentavos, '')) * 100);
+
+    const stripePriceData = {
+        productId: stripeProduct.id,
+        unit_amount: feeInCents 
+    }
+
+    const stripePrice = await createPrice(stripePriceData)
+
+    const newProfessional = new ProfessionalModel({
+      user: userId,
+      description: description,
+      fee: [{price_id: stripePrice.id, product_id: stripeProduct.id, fee: fee, productName: stripeProductData.name}],
+      experience: experience,
+    });
+    await newProfessional.save();
+
+    return response(res, 201, { message: 'Registro exitoso', professional: newProfessional });
     },
 
   getPendingProfessionals: async (req, res) => {
@@ -44,12 +65,28 @@ module.exports = {
       if (!professional) {
         return response(res, 404, { error: 'Profesional no encontrado' });
       }
-      const { description, fee, experience } = req.body;
+      const { description, fee, experience, addresses, profilePic, country, province } = req.body;
 
+      if (fee) {
+        const separadorCentavos = fee.includes(',') ? ',' : '.';
+
+        const feeInCents = parseInt(parseFloat(fee.replace(separadorCentavos, '')) * 100);
+
+        const newFee = {
+          fee: feeInCents, 
+          priceId: professional.fee.price_id
+        }
+        professional.fee.price_id = await editPrice(newFee).id
+      } else {
+        professional.fee = professional.fee;
+      }
       professional.description = description || professional.description;
-      professional.fee = fee || professional.fee;
       professional.experience = experience || professional.experience;
-
+      professional.description = addresses || professional.addresses;
+      professional.description = profilePic || professional.profilePic;
+      professional.description = country || professional.country;
+      professional.description = province || professional.province;
+      
       await professional.save();
 
       return response(res, 200, { message: 'Profesional actualizado', professional });
