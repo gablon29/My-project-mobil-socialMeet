4b9f11170118163c083c258b71b7cdf442db47fa
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-07-31 15:10:14 -0600

    Flujo de registro de veterinario autonomo y clinica

---
+++ Client/src/Components/ProfessionalArea/Register/FormRegister.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import Btn from "../../Buttons/ButtonCuston";
+import countrys from "../../../../extras/countrys.json";
+import { SelectList } from "react-native-dropdown-select-list";
+import cruz from '../../../../images/iconos/cruz.png';
+import { useImage } from "../../../CustomHooks/useImage";
+
+
+const FormRegister = ({modalidad, country, setCountry, setProvince, setRender}) => {
+
+    const [countryOptions, setCountryOptions] = useState([]);
+    const [provinceOptions, setProvinceOptions] = useState([]);
+    const [currentProvinces, setCurrentProvinces] = useState([]);
+    const {url, setUrl, uploadImage} = useImage();
+    
+    useEffect(() => {
+        const countries = countrys.map((country) => country.name);
+        setCountryOptions(countries);
+        console.log(countryOptions)
+      }, []);
+      
+    useEffect(() => {
+        if (country) {
+          const selectedCountry = countrys.find((c) => c.name === country);
+          if (selectedCountry) {
+            const provinces = selectedCountry.provinces;
+            setProvinceOptions(provinces);
+            setCurrentProvinces(provinces);
+          }
+        }
+    }, [country]);
+
+    const Label =  (title,set, value, text) => (<Text className="font-semibold text-base left-4 mt-5">{title} {set === value ? text : null}</Text>);
+    const Input = () => (<TextInput className="w-full shadow-lg shadow-black h-10 pl-3 rounded-lg bg-new"/>);
+
+    return (
+        <View className="h-full w-screen border items-center">
+            <Text className="text-2xl font-bold my-10">{modalidad}</Text>
+            <View className="w-10/12">
+                {Label("Nombre ", modalidad, "Clínica Veterinaria", "de la clínica")}
+                {Input()}
+                { modalidad === "Veterinario Autónomo" && <>{Label("Apellidos")}{Input()}</> }
+                {Label("País")}
+                <SelectList
+                    data={countryOptions}
+                    setSelected={setCountry}
+                    placeholder="Seleccionar"
+                    search={false}
+                    boxStyles={{
+                    backgroundColor: '#FEC89A',
+                    borderRadius: 10,
+                    borderColor: '#FEC89A',
+                    width: '100%'
+                    }}
+                    inputStyles={{
+                    fontSize: 12,
+                    fontFamily:"Poppins"
+                    }}
+                    dropdownStyles={{
+                    backgroundColor: '#FEC89A',
+                    }}
+                />
+                {Label("Provincia")}
+                <SelectList
+                    data={provinceOptions}
+                    setSelected={setProvince}
+                    placeholder="Seleccionar"
+                    search={false}
+                    boxStyles={{
+                    backgroundColor: '#FEC89A',
+                    borderRadius: 10,
+                    borderColor: '#FEC89A',
+                    fontFamily:"Poppins",
+                    width: '100%'
+                    }}
+                    inputStyles={{
+                    fontSize: 12
+                    }}
+                    dropdownStyles={{ backgroundColor: '#FEC89A' }}
+                />
+                {Label("Localidad")}
+                {Input()}
+                {modalidad === "Clínica Veterinaria" ? <>{Label("Calle y número")}{Input()}</> : null}
+                {modalidad === "Clínica Veterinaria" ? <>{Label("CIF / Num Identificación Fiscal")}{Input()}</> : null}
+                {Label("Teléfono")}
+                {Input()}
+                {Label("Email")}
+                {Input()}
+                {modalidad === "Veterinario Autónomo" ? <>{Label("Selecciona documento de identidad")}{Input()}</> : null}
+                {modalidad === "Veterinario Autónomo" ? 
+                <>{Label("Adjunta imagen de el documento")}
+                <TouchableOpacity className="flex justify-center items-center w-full h-32 bg-new rounded-lg" onPress={() => uploadImage()}>
+                    <Image source={url ? {uri: url} : cruz} style={url ? { width: 160, height: 160 } : {width: 50, height: 50 }} className="rounded-full" />
+                </TouchableOpacity>
+                </> 
+                : null}
+            </View>
+                <Btn 
+                    title={"Siguiente"}
+                    titleClass={"text-naranja font-bold text-base"}
+                    buttonClass={"my-10 bg-white border-2 border-naranja w-64 h-14 rounded-2xl items-center justify-center"}
+                    onPress={()=>setRender(5)}
+                />
+        </View>
+    );
+}
+ 
+export default FormRegister;
--- Client/src/Components/ProfessionalArea/Register/RegisterProfessional.jsx
+++ Client/src/Components/ProfessionalArea/Register/RegisterProfessional.jsx
@@ -4,18 +4,23 @@ import { useEffect, useState } from "react";
 import Politics from "./Politics";
 import RecordMode from "./RecordMode";
 import FormRegister from "./FormRegister";
+import { useProfesional } from "../../../CustomHooks/useProfesional";
+import VerificationMessage from "./VerifyMessaje";
 
 const RegisterProfessional = ({route}) => {
     const {register, text} = route.params;
     const [render, setRender] = useState(1);
+    const {tipo, setTipo, modalidad, setModalidad, setNombre, nombre, country, setCountry, province, setProvince, city, setCity, address, setAddress, phone, setPhone, documento, setDocumento,      fotoDoc, setFo, mascotasCuidar, setMascotaCuidar, fechaNacimiento, setFechaNacimiento, modalidadNoVet, setModalidadNoVet, description, setDescription, onSubmit
+    } = useProfesional();
 
     return (
         <ScrollView className="bg-white">
             <View>
-                {render === 1 && <SelectProfessionalArea setData={setData} data={data} register={register} text={text} setRender={setRender} render={render}/>}
-                {render === 2 && <Politics setRender={setRender} render={render}/>}
-                {render === 3 && <RecordMode />} {/* Debe entrar aquí solo si escoje veterinario en la seleccion de area profesional */}
-                {render === 4 && <FormRegister />}
+                {render === 1 && <SelectProfessionalArea setTipo={setTipo} register={register} text={text} setRender={setRender} render={render}/>}
+                {render === 2 && <Politics tipo={tipo} setRender={setRender} render={render}/>}
+                {render === 3 && <RecordMode setModalidad={setModalidad} setRender={setRender}/>}
+                {render === 4 && <FormRegister modalidad={modalidad} setRender={setRender} country={country} setCountry={setCountry} setProvince={setProvince} province={province}/>}
+                {render === 5 && <VerificationMessage setRender={setRender}/>} 
             </View>
         </ScrollView>
     );
