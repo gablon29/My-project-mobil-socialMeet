15f49bea24c2e7c48057f493ec796bb502a3c09f
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-08-06 01:37:46 +0200

    Profesional Area fix

--- Client/src/config/env.js
+++ Client/src/config/env.js
@@ -13,7 +13,7 @@
 //export const BACK_URL = 'http://192.168.0.12:8080'; // Rodri
 
 // export const BACK_URL= 'http://192.168.1.2:8080'; // Vini
-//export const BACK_URL= 'http://192.168.178.211:8080'; // santiago
+export const BACK_URL= 'http://192.168.178.211:8080'; // santiago
 // export const BACK_URL = 'http://192.168.0.8:8080'// Lauty
 
 // DESARROLLO
@@ -22,7 +22,7 @@
 
 //PRODUCCIÓN - HAY Q HACER UN REVERSE PROXY PARA MAPEAR PETICIONES DE HTTP/S -> a diferentes rutas.
     //nginx
-    export const BACK_URL= 'https://whopaws-production.up.railway.app';
+    // export const BACK_URL= 'https://whopaws-production.up.railway.app';
     export const BACK_SOCKET_URL = 'https://whopaws-production.up.railway.app/socketio'
 
 
