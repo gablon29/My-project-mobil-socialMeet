5edccaac8c54c0f0617b4f3ed36d1085025188da
LautyFarias247 <lautarofarias743@gmail.com>
2023-08-10 15:21:34 -0300

    useServices custom hook

--- Client/src/CustomHooks/useServices.jsx
+++ Client/src/CustomHooks/useServices.jsx
@@ -1,15 +1,18 @@
 import React,{ useState } from "react";
 import { CreateProfessionalServices,EditProfessionalMethod } from "../metodos/professionalMetodos";
-import { useSelector } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
+import { setErrorProfessional,setLoadingProffesional,setProfessional } from "../Redux/ReducerProffesional";
+import { useNavigation } from "@react-navigation/native";
 
 export const useServices = () => {
+	const dispatch = useDispatch()
+	const navigation = useNavigation()
 
 	const profession = useSelector((state) => state?.ReducerProfessional?.profession)
 	const professionalPets = useSelector((state) => state?.ReducerProfessional?.userProfessional?.professions[profession].mascotasAcuidar)
 
 	const [services,setServices] = useState([])
 	const [petsPerNight,setPetsPerNight] = useState({})
-	const [mascotaAcuidar,setmascotaAcuidar] = useState([])
 	const [mascotasAcuidarStrings,setMascotasAcuidarStrings] = useState([...professionalPets])
 	const saveServices = () => {
 		console.log("services",services);
@@ -24,19 +27,19 @@ export const useServices = () => {
 		// 	}
 		// }
 
-		// EditProfessionalMethod({
-		// 	data: { mascotasAcuidar: mascotasAcuidarStrings,profession },
-		// 	success: (updatedProfessional) => { dispatch(setProfessional(updatedProfessional)); navigation.goBack() },
-		// 	error: (e) => dispatch(setErrorProfessional(e)),
-		// 	loading: (boolean) => dispatch(setLoadingProffesional(boolean))
-		// })
+		EditProfessionalMethod({
+			data: { mascotasAcuidar: mascotasAcuidarStrings,profession },
+			success: (updatedProfessional) => { dispatch(setProfessional(updatedProfessional)); navigation.goBack() },
+			error: (e) => dispatch(setErrorProfessional(e)),
+			loading: (boolean) => dispatch(setLoadingProffesional(boolean))
+		})
 
-		// CreateProfessionalServices({
-		// 	services,
-		// 	success: (response) => { console.log(response); },
-		// 	error: (e) => { console.log(e); },
-		// 	loading: () => { }
-		// })
+		CreateProfessionalServices({
+			services,
+			success: (response) => { console.log(response); },
+			error: (e) => { console.log(e); },
+			loading: () => { }
+		})
 	}
 
 	return {
