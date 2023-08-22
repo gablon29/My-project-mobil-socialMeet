5d3c84a4e1746a18574afdbb7b235c7fcb1611e2
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-08-22 14:42:02 -0600

    fix env

--- Client/src/config/env.js
+++ Client/src/config/env.js
@@ -16,13 +16,8 @@
 // export const BACK_URL= 'http://192.168.178.211:8080'; // santiago
 // export const BACK_URL = 'http://192.168.0.8:8080'// Lauty
 
-<<<<<<< HEAD
  export const BACK_URL= 'http://192.168.1.3:8080' // Vini
 // export const BACK_URL= 'http://192.168.178.211:8080'; // santiago
-=======
-// export const BACK_URL= 'http://192.168.1.2:8080' // Vini
-export const BACK_URL= 'http://192.168.1.165:8080'; // santiago
->>>>>>> main
 
 //export const BACK_URL = 'http://192.168.0.8:8080'// Lauty
 
@@ -32,13 +27,8 @@ export const BACK_URL= 'http://192.168.1.165:8080'; // santiago
 
 //PRODUCCIÓN - HAY Q HACER UN REVERSE PROXY PARA MAPEAR PETICIONES DE HTTP/S -> a diferentes rutas.
     //nginx
-<<<<<<< HEAD
     //export const BACK_URL= 'https://whopaws-production.up.railway.app';
     //export const BACK_SOCKET_URL = 'https://whopaws-production.up.railway.app/socketio'
-=======
-   // export const BACK_URL= 'https://whopaws-production.up.railway.app';
-    export const BACK_SOCKET_URL = 'https://whopaws-production.up.railway.app/socketio'
->>>>>>> main
 
 
 
